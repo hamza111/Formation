f9885b813f39868f1cbaf9321a6a3b01d685673a
Hassen Oumerabet <hassen.oumerabet@gmail.com>
2019-03-29 09:21:51 +0100

    Commit dans un café

--- src/main/java/BookService.java
+++ src/main/java/BookService.java
@@ -3,6 +3,9 @@ import java.util.stream.Collectors;
 
 public class BookService {
 
+    // Methode qui filtre la liste des livre par année de pkublication
+    // ensuite trie le résultat oar prix d'une façon descendante
+    // Si les pris sont égaux, on trie par le nom de l'auteur
     public static List<Book> filterBooksByPublicationYearAndFilterByPriceTheFilterByAuthorName(List<Book> books) {
 
         return books.stream()
@@ -11,10 +14,12 @@ public class BookService {
                 .collect(Collectors.toList());
     }
 
+    // Retourne la liste des pays des auteurs sans doublons
     public static Set<String> getAuthorCountriesDistinct(List<Book> bookList) {
         return bookList.stream().map( b -> b.getAuthor().getCountry().getName() ).collect(Collectors.toSet());
     }
 
+    // Retourner un mpa qui contient la somme des prix des livres par catégorie
     public static Map<Catergory, Integer> getSumPriceGroupedByCategory(List<Book> bookList) {
         return bookList.stream().collect(Collectors.toMap(Book::getCatergory, Book::getPrice, (a, b) -> a + b));
     }
